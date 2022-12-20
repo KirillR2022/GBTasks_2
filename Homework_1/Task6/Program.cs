92ed2b0ee1edb791ce9e9198c9b4201675968794
Kirill <kirill075@mail.ru>
2022-12-20 18:39:09 +0300

    Решение Задачи 6

--- Homework_1/Task6/Program.cs
+++ Homework_1/Task6/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+int a = int.Parse(ReadLine()!);
+
+
+if (a % 2 == 0)
+{
+    Write("Да");
+}    
+else
+{
+    Write("Нет");
+}
