b440e702839fd1cd5c2930a04b068cac6904ccdc
Kirill <kirill075@mail.ru>
2022-12-20 18:31:05 +0300

    Решение Задачи 4

--- Homework_1/Task4/Program.cs
+++ Homework_1/Task4/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+int a = int.Parse(ReadLine()!);
+int b = int.Parse(ReadLine()!);
+int c = int.Parse(ReadLine()!);
+
+if (a > b && a > c)
+{
+    Write($"{a}");
+}
+if (b > a && b > c)
+{
+    Write($"{b}");
+}
+else
+{
+    Write($"{c}");
+}
