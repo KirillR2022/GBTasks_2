9845f7520dcacdacadc47a420e1d2b853149de1d
Kirill <kirill075@mail.ru>
2022-12-25 16:28:10 +0300

    Решение Задачи 10

---
+++ Homework_2/Task10/Program.cs
@@ -0,0 +1,6 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+using static System.Console;
+int number = int.Parse(ReadLine()!);
+int a = number/10;
+Write(a%10);
