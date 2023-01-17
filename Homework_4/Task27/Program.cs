4ccc5c574edbb8e1d14a91df6f5cabdfaa0216d2
Kirill <kirill075@mail.ru>
2023-01-17 22:40:55 +0300

    Решение Задачи 27

---
+++ Homework_4/Task27/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 27: Напишите программу, реализующую метод, который принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+
+using static System.Console;
+
+int A = int.Parse(ReadLine()!);
+
+WriteLine(($"{A} = {Summa(A)}"));
+
+
+int Summa(int number)
+{ 
+    int result = 0;
+    
+    while (number > 0)
+    {
+        result += number % 10;
+        number /= 10;
+    }
+
+
+    return result;
+
+}
