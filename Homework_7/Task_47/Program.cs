f50da2cae40b8877f1e5ec34f18b77c2383788d9
Kirill <kirill075@mail.ru>
2023-01-28 20:18:01 +0300

    Решение Задачи 47

---
+++ Homework_7/Task_47/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 47. Напишите программу реализующую метод, который возвращает
+// массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+using static System.Console;
+
+Clear();
+WriteLine("Введите значения: ");
+int[] intParams = Array.ConvertAll(ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries), int.Parse);
+double[,] array = GetMatrixArray(intParams[0], intParams[1], intParams[2], intParams[3]);
+PrintMatrixArray(array);
+
+double[,] GetMatrixArray(int rows, int columns, double minValue, int maxValue)
+{
+    Random rnd = new Random();
+    double[,] resultArray = new double[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            resultArray[i, j] = minValue + rnd.NextDouble() * ((maxValue - minValue));
+        }
+    }
+    return resultArray;
+}
+
+void PrintMatrixArray(double[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i, j],5:f1} ");
+        }
+        WriteLine();
+    }
+}
