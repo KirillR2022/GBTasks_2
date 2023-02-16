2ed02a4021b9a5a383d9a0a1ac18cc36d151e32e
Kirill <kirill075@mail.ru>
2023-02-16 23:33:33 +0300

    ipr

---
+++ IPR/Program.cs
@@ -0,0 +1,32 @@
+﻿using static System.Console;
+WriteLine("Введите массив через пробел: ");
+string[] arr = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+WriteLine();
+Console.WriteLine("[{0}]", string.Join(", ", SelectArr(arr)));
+
+string[] SelectArr(string[] inArr) 
+{
+    string[] resultArr = new string[NewArrSize(inArr)];
+    int count = 0;
+    foreach (var item in inArr)
+    {
+        if (item.Length <= 3)
+        {
+            resultArr[count] = item;
+            count++;
+        }
+    }
+    return resultArr;
+
+}
+
+int NewArrSize(string[] inArr) 
+{
+    int result = 0;
+    foreach (var item in inArr)
+    {
+        if (item.Length <= 3)
+            result++;
+    }
+    return result;
+}
